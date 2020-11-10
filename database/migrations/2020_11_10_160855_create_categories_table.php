3015f7f4b132e8ea11af8b08066ab53487c6143d
Muhammad <muhammad.azam@dmdmax.com>
2020-11-10 21:21:00 +0500

    initial commit - Project Setup

---
+++ database/migrations/2020_11_10_160855_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateCategoriesTable.
+ */
+class CreateCategoriesTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('categories', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parentId')->unsigned();
+            $table->string('title');
+            $table->string('metaTitle')->nullable();
+            $table->string('slug');
+            $table->string('content')->nullable();
+            $table->timestamps();
+
+            $table->foreign('parentId', 'categories_parentId_foreign_key')->references('id')->on('categories')->onDelete('cascade');
+        });
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('categories');
+	}
+}
